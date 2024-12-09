7133e220c662882a70c9b25c11d68563afa6b1f7
Ezeh_Ginnikachi <145734262+Smallvillecyberspace@users.noreply.github.com>
2024-12-09 04:40:45 -0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,29 +1,34 @@
-const Header = () => {
-  const course = "Half Stack application development";
-  return <h1>{course}</h1>;
-};
+import React from "react";
 
-const Content = () => {
-  const exercises = [
-    { id: 1, part: "Fundamentals of React", exercise: 10 },
-    { id: 2, part: "Using props to pass data", exercise: 7 },
-    { id: 3, part: "State of a component", exercise: 14 },
-  ];
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        id: 1,
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      { id: 2, name: "Using props to pass data", exercises: 7 },
+      { id: 3, name: "State of a component", exercises: 14 },
+    ],
+  };
 
-  const listItems = exercises.map((exercise) => (
-    <li key={exercise.id}>
-      {exercise.part}: &nbsp; <b>{exercise.exercise}</b>
-    </li>
-  ));
-
-  const totalExercises = exercises.reduce(
-    (acc, current) => acc + current.exercise,
+  const totalExercises = course.parts.reduce(
+    (acc, current) => acc + current.exercises,
     0
   );
-  console.log(totalExercises);
+
   return (
     <div>
-      <ul>{listItems}</ul>
+      <ul>
+        <h1>{course.name}</h1>
+        {course.parts.map((part) => (
+          <li key={part.id}>
+            {part.name}: &nbsp; {part.exercises}
+          </li>
+        ))}
+      </ul>
       <Total total={totalExercises} />
     </div>
   );
@@ -31,17 +36,6 @@ const Content = () => {
 
 const Total = ({ total }) => {
   return <ul> Total exercises: {total}</ul>;
-};
-
-const App = () => {
-  // const-definitions
-
-  return (
-    <div>
-      <Header />
-      <Content />
-    </div>
-  );
 };
 
 export default App;
